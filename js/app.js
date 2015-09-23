8e638b538e7eac5eebf2f131a3f8786b01a13881
Isaac Gasi <isaac.gasi@gmail.com>
2015-09-23 07:00:14 -0500

    Added requirejs, config .bowerrc, define improve skelleton for backbone with requierejs, redefine app, router, main and require

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log("Ready for use, your App is preconfigured with Backbone");
+// Filename: app.js
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'router', // Request router.js
+], function($, _, Backbone, Router){
+    var initialize = function(){
+
+        // Custom Options
+        var config = {
+            //Config base URL for access to REST API
+            "base_url": "http://45.55.24.20/",
+            //Define debug mode
+            "debug": true,
+        };
+
+        // Pass in our Router module and call it's initialize function
+        Router.initialize(config);
+
+    };
+
+    return {
+        initialize: initialize
+    };
+});
---
+++ js/router.js
@@ -0,0 +1,69 @@
+// Filename: router.js
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'views/home/HomeView',
+    'views/projects/ProjectsView',
+    'views/contributors/ContributorsView',
+    'views/footer/FooterView'
+], function($, _, Backbone, HomeView, ProjectsView, ContributorsView, FooterView) {
+
+    var AppRouter = Backbone.Router.extend({
+
+        //Default configuration settings
+        config:  {
+            //Config base URL for access to REST API
+            "base_url": "http://45.55.24.20/",
+            //Define debug mode
+            "debug": false,
+        },
+
+        routes: {
+            //home
+            '': 'function',
+
+            // Default
+            '*actions': 'defaultAction'
+        },
+
+        initialize: function( config ){
+            this.config = config;
+        },
+
+
+    });
+
+
+    var initialize = function(){
+
+        var app_router = new AppRouter;
+
+        app_router.on('route:', function(){
+
+            // Call render on the module we loaded in via the dependency array
+            //var projectsView = new ProjectsView();
+            //projectsView.render();
+
+        });
+
+        app_router.on('route:defaultAction', function (actions) {
+
+            // We have no matching route, lets display the home page
+            //var homeView = new HomeView();
+            **homeView.render();
+        });
+
+        // Unlike the above, we don't call render on this view as it will handle
+        // the render call internally after it loads data. Further more we load it
+        // outside of an on-route function to have it loaded no matter which page is
+        // loaded initially.
+        //var footerView = new FooterView();
+
+        Backbone.history.start();
+    };
+
+    return {
+        initialize: initialize
+    };
+});
